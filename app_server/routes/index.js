b971a5d350202de15e05ffebbab83cf4255cafd3
WillieYang <849346604@qq.com>
2017-04-06 16:11:30 +0100

    Adding controllers

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var ctrlLocation = require('../controllers/location');
+var ctrlLocation = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
-/* GET Location page. */
+/* Location page. */
 router.get('/', ctrlLocation.homelist);
-router.get('/location', ctrlLocation.locationinfo);
+router.get('/location', ctrlLocation.locationInfo);
 router.get('/location/review/new', ctrlLocation.addReview);
-/* GET Others page. */
+/* Others page. */
 router.get('/about', ctrlOthers.about);
 
 module.exports = router;
