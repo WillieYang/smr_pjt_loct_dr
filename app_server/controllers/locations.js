4eba7b0074f75892a80d80464376ed00600e29e5
Sheng Yang <849346604@qq.com>
2017-06-02 09:02:30 +0100

    Preparation for passing data from controller to view

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,30 @@
 /* Get 'home' page. */
 module.exports.locationList = function(req, res){
-	res.render('locations_list', {title: 'Location list'})
+	res.render('locations_list', {
+		title: 'Find places to work near you!',
+		pageHeader: {
+			title: 'Find places to work near you!'
+		},
+		locations: [{
+			name: 'Starbucks',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 3,
+			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+			distance: '100m'
+		},{
+			name: 'Cafe Hero',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 4,
+			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+			distance: '200m'
+		},{
+			name: 'Burger Queen',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 2,
+			facilities: ['Food', 'Premium wifi'],
+			distance: '250m'
+		}] 
+	})
 };
 
 /* Get 'location infomation' page. */
