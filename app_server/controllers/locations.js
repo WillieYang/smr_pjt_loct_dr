a90c4b48eb82ee4c295e83de06ee50fa38786063
Sheng Yang <849346604@qq.com>
2017-06-03 18:09:18 +0100

    Transformation of all the view and controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -19,7 +19,7 @@ module.exports.locationList = function(req, res){
 			distance: '200m'
 		},{
 			name: 'Burger Queen',
-			address: '154 Green Street, Newcastle Upton Tyne, NE12 2DB',
+			address: '154 Green Street, Newcastle Upton Tyne, NE12 2DB		',
 			rating: 2,
 			facilities: ['Food', 'Premium wifi'],
 			distance: '250m'
@@ -29,10 +29,54 @@ module.exports.locationList = function(req, res){
 
 /* Get 'location infomation' page. */
 module.exports.locationInfo = function(req, res){
-	res.render('location_info', {title: 'location Infomation'})
+	res.render('location_info', {
+		title: 'Sainsbury Local',
+		pageHeader: {
+			title: 'Sainsbury Local'
+		},
+		sidebar: {
+			context: 'provide accessible wifi and space to help you sit down with laptop and get work done.',
+			callToAction: "If you've been and you like it - or if you don't - please leave a review to help other people just like you."
+		},
+		location: {
+			name: 'Sainsbury\'s Local',
+			address: '301 Burgess Road, Southampton, SO16 3BA',
+			rating: 3,
+			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+			coords: {lat: 50.938497, lng: -1.390814},
+			openingTimes: [{
+				days: 'Monday - Friday',
+				opening: '7:00am',
+				closing: '7:00pm',
+				closed: false
+			},{
+				days: 'Saturday',
+				opening: '8:00am',
+				closing: '5:00pm',
+				closed: false
+			},{
+				days: 'Sunday',
+				closed: true
+			}],
+			reviews: [{
+				author: 'Jack Lee',
+				rating: 5,
+				timestamp: '16 May 2017',
+				reviewText: 'Quite, and the coffee is yummy!!!'
+				},{
+				author: 'Willie Rathke',
+				rating: 3,
+				timestamp: '27 May 2017',
+				reviewText: 'Suitable for studying, haha!!!'
+			}]
+		}
+	})
 };
 
 /* Get 'Add Review' page. */
 module.exports.addReview = function(req, res){
-	res.render('location_review_form', {title: 'Add Review'})
+	res.render('location_review_form', {
+		title: 'Review Sainsbury',
+		pageHeader: {title: 'Review Sainsbury'}
+	})
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -5,6 +5,20 @@ module.exports.homepage = function(req, res){
 
 /* Get 'about' page. */
 module.exports.about = function(req, res){
-	res.render('about', {title: 'About'})
+	res.render('about', {
+		title: 'About',
+		content: 'This is a recommendation website created which is used to provide \
+			users some nearby appropriate restaurants or shops.\n\n In fact, this web\
+			application is part of my summer project, and the aim is to build the \
+			application using MEAN stack, and comparing it with LAMP. \n\nIn this \
+			specific project, I would use MEAN stack(AngularJS as client-side framework,\
+			Express.js as server-side web-application framework, Node.js as server-side \
+			platform and MongoDB as database) to develop that E-business web site, and \
+			comparing all the components in MEAN stack with other corresponding techs in\
+			the area of front and back end, server and database to improve user’s \
+			experience to some degree. For instance, the pros and cons in relational\
+			 database and NoSQL, the advantageous when using Node.js as server.'
+
+	})
 };
 
