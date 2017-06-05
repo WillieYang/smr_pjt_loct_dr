77162715a25c47d64c104c3a2e98db07d842ab25
Sheng Yang <849346604@qq.com>
2017-06-05 16:55:20 +0100

    Add the path of Schema and coordinates using longitude and latitude

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -2,8 +2,9 @@
 var mongoose = require('mongoose');
 
 var locationSchema = new mongoose.Schema({
-	name: String,
+	name: {type: String, required: true},
 	address: String,
-	rating: Number,
-	facilities: [String]
+	rating: {type: Number, "default": 0, min: 0, max: 5},
+	facilities: [String],
+	coords: {type: {Number}, index: '2dsphere'}
 });
