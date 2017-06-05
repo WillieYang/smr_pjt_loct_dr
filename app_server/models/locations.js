a245fbc3372193de4f3fd726e845700646bfaa53
Sheng Yang <849346604@qq.com>
2017-06-05 18:15:29 +0100

    Add the nested Schema for the Schema location

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,10 +1,29 @@
 // Require the Mongoose
 var mongoose = require('mongoose');
 
+// Schema for Opening Time (Nested Schema of locationSchema)
+var openingTimeSchema = new mongoose.Schema({
+	days: {type: String, required: true},
+	opening: String,
+	closing: String,
+	closed: {type: Boolean, required: true}
+});
+
+// Schema for Reviews (Nested Schema of locationShema)
+var reviewSchema = new mongoose.Schema({
+	author: String,
+	rating: {type: Number, "default": 0, min: 0, max: 5},
+	reviewText: String,
+	createdOn: {type: Date, "default": Date.now} 
+});
+
+// Schema for location
 var locationSchema = new mongoose.Schema({
 	name: {type: String, required: true},
 	address: String,
 	rating: {type: Number, "default": 0, min: 0, max: 5},
 	facilities: [String],
-	coords: {type: {Number}, index: '2dsphere'}
+	coords: {type: [Number], index: '2dsphere'},
+	openingTime: [openingTimeSchema],
+	reviews: [reviewSchema]
 });
