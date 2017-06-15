8433f8a94cbefade7a5faeae3331fa105a6797d3
Sheng Yang <849346604@qq.com>
2017-06-15 09:49:54 +0100

    Remove the log

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
 	dbURI = process.env.MONGODB_URI;
 	console.log('test the dbURI:' + dbURI);
 }
-var dbURIlog = 'mongodb://localhost/smr_pjt_loctog';
+// var dbURIlog = 'mongodb://localhost/smr_pjt_loctog';
 
 // Connect the database using Mongoose
 var logDB = mongoose.createConnection(dbURIlog);
@@ -35,13 +35,13 @@ mongoose.connection.on('disconnected', function() {
 });
 
 // Monitoring the connection with Mongoose connection events(Created database)
-logDB.on('connected', function (){
-	console.log('Mongoose connected to ' + dbURIlog);
-});
+// logDB.on('connected', function (){
+// 	console.log('Mongoose connected to ' + dbURIlog);
+// });
 
-logDB.close(function () {
-	console.log('Mongoose log disconnected');
-});
+// logDB.close(function () {
+// 	console.log('Mongoose log disconnected');
+// });
 
 // Create a function to shutdown the connection.
 var gracefulShutdown = function(msg, callback){
