66bcd27db8daf42ff966dc6c0ebea79ab4581728
Sheng Yang <849346604@qq.com>
2017-06-16 17:54:40 +0100

    Revise about the URI of api

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -12,9 +12,9 @@ router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
 
 // reviews
 router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
-router.get('/locations/:locationid/:reviewid', ctrlReviews.reviewsReadOne);
-router.put('/locations/:locationid/:reviewid', ctrlReviews.reviewsUpdateOne);
-router.delete('/locations/:locationid/:reviewid', ctrlReviews.reviewsDeleteOne);
+router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
+router.put('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
+router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
 
 // export routes
 module.exports = router;
