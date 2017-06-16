05ea90291404f53a85d1e2736241b6a86b5b94f2
Sheng Yang <849346604@qq.com>
2017-06-16 16:42:17 +0100

    Add a controllers folder containing locations.js file and reviews.js file

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,9 @@
+module.exports.locationCreate = function(req, res){};
+
+module.exports.locationListByDistance = function(req, res){};
+
+module.exports.locationReadOne = function(req, res){};
+
+module.exports.locationUpdateOne = function(req, res){};
+
+module.exports.locationDelete = function(req, res){};
