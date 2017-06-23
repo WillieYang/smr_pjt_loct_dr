930c91e04e8d201c03ee490f14522406e39bb667
Sheng Yang <849346604@qq.com>
2017-06-23 14:16:04 +0100

    adding and save a subdocument

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -9,7 +9,47 @@ var sendJsonResponse = function(res, status, content){
 
 // create
 module.exports.reviewsCreate = function(req, res){
-	sendJsonResponse(res, 200, {"status": "success"});
+	var locationid = req.params.locationid;
+	if (locationid) {
+		Location
+		  .findById(locationid)
+		  .select('reviews')
+		  .exec(
+		  	function(err, location){
+		  		if (err) {
+		  			sendJsonResponse(res, 400, err);
+		  		} else {
+		  			addReview(req, res, location);
+		  		}
+		  	}
+		  );
+	} else {
+		sendJsonResponse(res, 404, {
+			"message": "Not found, locationid required!!!"
+		});
+	}
+};
+
+var addReview = function(req, res, location) {
+  if (!location) {
+    sendJsonResponse(res, 404, "locationid not found");
+  } else {
+	  location.reviews.push({
+	      author: req.body.author,
+	      rating: req.body.rating,
+	      reviewText: req.body.reviewText
+	  });
+	  location.save(function(err, location) {
+	      var thisReview;
+	      if (err) {
+	        sendJsonResponse(res, 400, err);
+	      } else {
+	        updateAverageRating(location._id);
+	        thisReview = location.reviews[location.reviews.length - 1];
+	        sendJsonResponse(res, 201, thisReview);
+	      }
+	    });
+  }
 };
 
 // get
