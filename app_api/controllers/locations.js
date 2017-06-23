495d8421b639f614bb7fd3fb2aa7a6cab30cccf3
Sheng Yang <849346604@qq.com>
2017-06-23 09:57:02 +0100

    Creating document in MongoDB

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -28,7 +28,29 @@ var theEarth = (function(){
 
 // create
 module.exports.locationsCreate = function(req, res){
-	sendJsonResponse(res, 200, {"status": "success"});
+	Location.create({
+		name: req.body.name,
+		address: req.body.address,
+		facilities: req.body.facilities.split(","),
+		coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+		openingTimes: [{
+			days: req.body.days1,
+			opening: req.body.opening1,
+			closing: req.body.closing1,
+			closed: req.body.closed1,
+		}, {
+			days: req.body.days2,
+			opening: req.body.opening2,
+			closing: req.body.closing2,
+			closed: req.body.closed2,
+		}]
+	}, function(err, location){
+		if (err) {
+			sendJsonResponse(res, 404, err);
+		}else {
+			sendJsonResponse(res, 201, location);
+		}
+	});
 };
 
 // get 
