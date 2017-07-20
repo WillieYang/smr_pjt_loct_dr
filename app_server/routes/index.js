9d0fd9bed1761c03be3253a96f920f3b3150c347
Sheng Yang <849346604@qq.com>
2017-07-20 11:57:07 +0100

    Change the jade syntax and then add the id and name for query

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var ctrlLocation = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
+var ctrlResults = require('../controllers/results');
 /* Location page. */
 router.post('/locationlist', ctrlLocation.locationList);
 // router.post('/locationlistPost', ctrlLocation.locationListPost);
@@ -11,5 +12,7 @@ router.post('/location/:locationid/reviews/new', ctrlLocation.addReview_post);
 /* Others page. */
 router.get('/', ctrlOthers.homepage);
 router.get('/about', ctrlOthers.about);
+/* Google search results page. */
+router.post('/results', ctrlResults.searchResults);
 
 module.exports = router;
