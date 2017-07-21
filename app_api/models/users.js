bfaebd6c5696dd4dda2608ec4f97724b46c0ae55
Sheng Yang <849346604@qq.com>
2017-07-21 20:01:20 +0100

    Add a User model with Mongoose

---
+++ app_api/models/users.js
@@ -0,0 +1,14 @@
+// Require the Mongoose
+var mongoose = require('mongoose');
+
+// Schema for user
+var UserSchema = new mongoose.Schema({
+	username: {type: String, required: true},
+	email: {type: String, required: true},
+	password: {type: String, required: true},
+});
+
+// Compiling the Schema into Model
+// First parameter is the name of model, 
+// and the second parameter is the name of schema.
+mongoose.model('User', UserSchema);
