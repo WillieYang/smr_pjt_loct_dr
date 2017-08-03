f9c2ae5d0e15addb2ffe836d30791787f4bd8dca
Sheng Yang <849346604@qq.com>
2017-08-03 10:06:17 +0100

    Complement of report collection in db

--- app_api/models/reports.js
+++ app_api/models/reports.js
@@ -7,6 +7,8 @@ var reportSchema = new mongoose.Schema({
 	whistlebower_email: String,
 	reviewAuthor_name: String,
 	whistlebower_email: String,
+	review_content: String,
+	report_content: String,
 	place_id: String,
 	review_id: String,
 	createdOn: {type: Date, "default": Date.now}
