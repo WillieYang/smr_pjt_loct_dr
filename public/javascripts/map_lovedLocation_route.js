aa4a02a910c5ef6491b3351d636d25b3e42811b1
Sheng Yang <849346604@qq.com>
2017-08-04 11:28:32 +0100

    Making use of Google maps api (directions) to provide users the route to theri loved location(upgraded version compared with polylines)

--- public/javascripts/map_lovedLocation_route.js
+++ public/javascripts/map_lovedLocation_route.js
@@ -1,6 +1,10 @@
 console.log("lat in included js file:" + typeof targetlat);
 console.log("lng in included js file:" + typeof targetlng);
 function initMap() {
+
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    
     if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = {
@@ -8,25 +12,35 @@ function initMap() {
         lng: position.coords.longitude
       };
       console.log("posLat:" + pos.lat);
+
       var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 18,
         center: {lat: (pos.lat+ targetlat)/2, lng: (pos.lng+ targetlng)/2},
         mapTypeId: 'terrain'
       });
 
-      var flightPlanCoordinates = [
-        {lat: pos.lat, lng: pos.lng},
-        {lat: targetlat, lng: targetlng}
-      ];
-      var flightPath = new google.maps.Polyline({
-        path: flightPlanCoordinates,
-        geodesic: true,
-        strokeColor: '#FF0000',
-        strokeOpacity: 1.0,
-        strokeWeight: 2
-      });
-
-      flightPath.setMap(map);
+      function calcRoute() {
+          var start = new google.maps.LatLng(pos.lat, pos.lng);
+          var end = new google.maps.LatLng(targetlat, targetlng);
+          var bounds = new google.maps.LatLngBounds();
+          bounds.extend(start);
+          bounds.extend(end);
+          map.fitBounds(bounds);
+          var request = {
+              origin: start,
+              destination: end,
+              travelMode: google.maps.TravelMode.WALKING
+          };
+          directionsService.route(request, function (response, status) {
+              if (status == google.maps.DirectionsStatus.OK) {
+                  directionsDisplay.setDirections(response);
+                  directionsDisplay.setMap(map);
+              } else {
+                  alert("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
+              }
+          });
+      }
+      calcRoute();
     });
   }
 }
